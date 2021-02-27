94efaf9e477fc5f013402b3337301d36c0281a9b
Salt Chang <saltchang.tw@gmail.com>
2021-02-27 14:15:49 +0800

    feat: add comment in router

--- router/routes.go
+++ router/routes.go
@@ -5,6 +5,8 @@ import "github.com/saltchang/magfile-server/handler"
 var h = handler.NewHandler(database)
 
 // Routes is the list of routes
+// key: pattern of the regex of path of the route
+// value: handler function of the route
 var Routes = map[string]interface{}{
 	"^/$":             h.HomeHandler,
 	"^/users(/)?$":    h.UsersHandler,
